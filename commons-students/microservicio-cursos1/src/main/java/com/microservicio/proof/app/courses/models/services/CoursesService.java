04097f627bcdba4596bb2c45e80b39a0c4bd7998
Alejandro <alejandro.molinar17@gmail.com>
2022-06-08 17:38:29 -0400

    Eliminacion de carpeta Clonada

--- commons-students/microservicio-cursos1/src/main/java/com/microservicio/proof/app/courses/models/services/CoursesService.java
+++
@@ -1,9 +0,0 @@
-package com.microservicio.proof.app.courses.models.services;
-
-import com.microservicio.proof.app.courses.models.entity.Courses;
-import com.microservicio.proof.commons.services.CommonService;
-
-public interface CoursesService extends CommonService<Courses>{
-	
-	public Courses findCouseByStudent(Long id);
-}
